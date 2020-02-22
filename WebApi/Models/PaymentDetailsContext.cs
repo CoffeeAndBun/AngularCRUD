a8e88b38de855d070a40a5b8d68cd358a9d2df44
Vijay <vijay.gangus@gmail.com>
2020-02-22 13:28:49 -0500

    Created WebApi Entities, DBContext and Migrations

---
+++ WebApi/Models/PaymentDetailsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models.Entities;
+
+namespace WebApi.Models
+{
+    public class PaymentDetailsContext : DbContext
+    {
+        public PaymentDetailsContext(DbContextOptions<PaymentDetailsContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
